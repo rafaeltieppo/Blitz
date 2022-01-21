72de0581b1a97f53969e039613159704e72d4c71
Rafael Tieppo <82878718+rafaeltieppo@users.noreply.github.com>
2022-01-21 11:32:29 -0300

    Backend Blitz

---
+++ backend/src/controller/UsuarioController.js
@@ -0,0 +1,58 @@
+const Usuario = require('../model/Usuario');
+
+const create = async (req, res) => {
+    const data = req.body;
+
+    const ret = await Usuario.create(data);
+
+    res.json(ret);
+}
+
+const read = async (req, res) => {
+    let filtro = {};
+
+    let id = req.params.id;
+
+    if(id != undefined) filtro = { where : { id: id }}
+
+    const ret = await Usuario.findAll(filtro);
+
+    res.json(ret);
+}
+
+const update = async (req, res) => {
+    const id = req.params.id;
+
+    const data = req.body;
+
+    let ret = await Usuario.update(data, {
+        where: { id: id }
+    });
+
+    ret = await Usuario.findAll({
+        where: { id: id }
+    });
+
+    res.json(ret);
+}
+
+const remove = async (req, res) => {
+    const id = req.params.id;
+
+    const ret = await Usuario.destroy({
+        where: { id: id }
+    })
+
+    if(ret == 1) {
+        res.json({id: id});
+    }else {
+        res.status(400);
+    }
+}
+
+module.exports = {
+    create,
+    read,
+    update,
+    remove
+}
